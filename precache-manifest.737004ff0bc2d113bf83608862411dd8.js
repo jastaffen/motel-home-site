014dcd0f24dd4ccdc2963f345fc72b714496d565
jastaffen <jack.staffen@gmail.com>
2020-03-23 20:48:55 -0400

    Updates

---
+++ precache-manifest.737004ff0bc2d113bf83608862411dd8.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e5866a37e73d97e8d9a1c89c530a37f",
+    "url": "/motel-home-site/index.html"
+  },
+  {
+    "revision": "c158b2980d6c566996ad",
+    "url": "/motel-home-site/static/css/main.0ca33828.chunk.css"
+  },
+  {
+    "revision": "559c7c6fe70bc2c6b9cd",
+    "url": "/motel-home-site/static/js/2.fabce287.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/motel-home-site/static/js/2.fabce287.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c158b2980d6c566996ad",
+    "url": "/motel-home-site/static/js/main.e93636c4.chunk.js"
+  },
+  {
+    "revision": "7842e5a4a6ff0ff3c3cc",
+    "url": "/motel-home-site/static/js/runtime-main.797bd3ba.js"
+  },
+  {
+    "revision": "2cf5ece36b23853d352b3fdea65828b2",
+    "url": "/motel-home-site/static/media/bulletpoint.2cf5ece3.png"
+  },
+  {
+    "revision": "69fcf1d5be502cff9c1139b5d9d8bfb4",
+    "url": "/motel-home-site/static/media/fisch.69fcf1d5.jpeg"
+  },
+  {
+    "revision": "d73a99f6bcea6c4681d2bdb2839a9cb9",
+    "url": "/motel-home-site/static/media/glossier.d73a99f6.jpeg"
+  },
+  {
+    "revision": "4842c011524b129b7094f0058381cf3b",
+    "url": "/motel-home-site/static/media/playboy.4842c011.jpeg"
+  },
+  {
+    "revision": "f1c743eb57d0a86361c0ce5f8a4e78e0",
+    "url": "/motel-home-site/static/media/verses2.f1c743eb.png"
+  },
+  {
+    "revision": "efcb7db5a8c75221f6ec199b6201f2e6",
+    "url": "/motel-home-site/static/media/vogueflowers.efcb7db5.png"
+  }
+]);
